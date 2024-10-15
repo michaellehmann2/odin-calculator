3809891a8a9de14ec6fc8c0cc46f5e53cc845c16
Michael Lehmann <mikelehmann00@gmail.com>
2024-10-15 15:59:09 -0500

    Add basic calc operations

---
+++ calculator.js
@@ -0,0 +1,36 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(num1, num2, op) {
+    switch (op) {
+        case 'add': 
+            return add(num1, num2);
+            break;
+        case 'subtract': 
+            return subtract(num1, num2);
+            break;
+        case 'multiply':
+            return multiply(num1, num2);
+            break;
+        case 'divide': 
+            return divide(num1, num2);
+            break;
+    }
+}
