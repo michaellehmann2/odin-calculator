f4e102cc3deac80bba7b4eaf6c69b5b3157a8d77
Michael Lehmann <mikelehmann00@gmail.com>
2024-10-16 16:47:45 -0500

    Add functionality for operators and equals buttons

--- calculator.js
+++ calculator.js
@@ -1,7 +1,11 @@
-let firstNumber;
-let secondNumber;
-let operator;
-let displayValue = '0';
+//represents the current value displayed on the screen
+let currentValue = '0';
+
+//represents the last operation button pressed
+let storedOperator = '';
+
+//represents the last value entered into the calculator before an operation button was pressed
+let storedValue = '';
 
 function add(num1, num2) {
     return num1 + num2;
@@ -20,6 +24,9 @@ function divide(num1, num2) {
 }
 
 function operate(num1, num2, op) {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
     switch (op) {
         case 'add': 
             return add(num1, num2);
@@ -36,21 +43,92 @@ function operate(num1, num2, op) {
     }
 }
 
-function updateDisplay(digit) {
-    if (displayValue === '0') {
-        displayValue = digit;
+function updateDisplay() {
+    display.textContent = currentValue;
+}
+
+// clearDisplay - clear button function;
+// set the currentValue to '0', and empty out the stored value and operation.
+// then, redraw the display
+function clearDisplay() {
+    currentValue = '0';
+    storedOperator = '';
+    storedValue = '';
+    updateDisplay();
+}
+
+// appendDigit? - number button
+// if the currentValue is a 0 (nothing has been typed), set currentValue to the digit
+// otherwise, append the digit to the end
+// then, redraw the display
+function appendDigit(digit) {
+    if (currentValue === '0') {
+        currentValue = digit;
     }
     else {
-        displayValue += digit;
+        currentValue += digit;
     }
-    display.textContent = displayValue;
+    updateDisplay();
+}
+
+// appendOperation - operator button function
+// if there's a stored value (e.g., you typed like 2 + 3 - or something)
+//   call compute() (to compute the 2 + 3 and store its result in currentValue)
+// store currentValue in storedValue
+// then, storedOperator = operator from button pressed
+// then, set currentValue to ''
+function appendOperator(operator) {
+    if (storedValue) {
+        compute();
+    }
+    storedValue = currentValue;
+    storedOperator = operator;
+    currentValue = '';
+}
+
+// compute - equals button function
+// if an operation is stored
+//   if there is a stored value
+//     call operate with storedValue, currentValue, and storedOperator
+//     store that result in currentValue
+//   else (no stored value)
+//     call operate with currentValue, currentValue, and storedOperator
+//     store that result in currentValue
+//   clear storedValue, storedOperator
+//   call updateDisplay
+function compute() {
+    if (storedOperator) {
+        if (storedValue === '') {
+            storedValue = currentValue;
+        }
+        currentValue = operate(storedValue, currentValue, storedOperator);
+        storedValue = '';
+        storedOperator = '';
+        updateDisplay();
+    }
 }
 
 const display = document.querySelector('.display');
-const numberButtons = Array.from(document.querySelectorAll('.number'));
 
+const numberButtons = document.querySelectorAll('.number');
 numberButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        updateDisplay(button.textContent);
+        appendDigit(button.textContent);
     })
+})
+
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', clearDisplay);
+
+const equalsButton = document.querySelector('.equals');
+equalsButton.addEventListener('click', compute);
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach((button) => {
+    let classes = button.className.split(' ');
+    let myOperation = classes[0];
+
+    button.addEventListener('click', () => {
+        appendOperator(myOperation);
+    });
 })
